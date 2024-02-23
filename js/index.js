dcb5da5bf0bb29b1325b8ce9a1f601b52ab72b3f
Daniel <oscar.d.gonzalez@gmail.com>
2024-02-23 17:08:21 -0500

    refactor: :recycle: created Slider class (OOP pattern)

--- js/index.js
+++ js/index.js
@@ -1,35 +1,50 @@
-const sliderContainer = document.querySelector('.slider-container');
-const slides = document.querySelectorAll('.slide');
-const previousButton = document.querySelector('.previous');
-const nextButton = document.querySelector('.next');
+class Slider {
+  constructor(containerSelector, slideSelector, previousButtonSelector, nextButtonSelector) {
+    this.sliderContainer = document.querySelector(containerSelector);
+    this.slides = document.querySelectorAll(slideSelector);
+    this.previousButton = document.querySelector(previousButtonSelector);
+    this.nextButton = document.querySelector(nextButtonSelector);
+    this.currentSlide = 0;
+    this.numberOfSlides = this.slides.length;
+    this.init();
+  }
 
-let currentSlide = 0;
-const numberOfSlides = slides.length;
+  init() {
+    this.disableButton();
+    this.addEventListeners();
+  }
 
-function disableButton() {
-  const isAtFirstSlide = currentSlide === 0;
-  const isAtLastSlide = currentSlide === numberOfSlides - 1;
+  disableButton() {
+    const isAtFirstSlide = this.currentSlide === 0;
+    const isAtLastSlide = this.currentSlide === this.numberOfSlides - 1;
 
-  previousButton.classList.toggle('disabled', isAtFirstSlide);
-  nextButton.classList.toggle('disabled', isAtLastSlide);
+    this.previousButton.classList.toggle('disabled', isAtFirstSlide);
+    this.nextButton.classList.toggle('disabled', isAtLastSlide);
+  }
+
+  transitionSlide() {
+    this.sliderContainer.style.transform = `translateX(-${this.currentSlide * 100}%)`;
+  }
+
+  addEventListeners() {
+    function handleButtonClick(direction) {
+      if (direction === 'previous') {
+        this.currentSlide--;
+      }
+      if (direction === 'next') {
+        this.currentSlide++;
+      }
+      this.transitionSlide();
+      this.disableButton();
+    }
+
+    this.previousButton.addEventListener('click', () => {
+      handleButtonClick.call(this, 'previous');
+    })
+    this.nextButton.addEventListener('click', () => {
+      handleButtonClick.call(this, 'next');
+    })
+  }
 }
 
-function transitionSlide() {
-  sliderContainer.style.transform = `translateX(-${currentSlide * 100}%)`;
-}
-
-window.addEventListener('load', () => {
-  disableButton();
-})
-
-previousButton.addEventListener('click', () => {
-  currentSlide--;
-  transitionSlide();
-  disableButton();
-})
-
-nextButton.addEventListener('click', () => {
-  currentSlide++;
-  transitionSlide();
-  disableButton();
-})
+const slider = new Slider('.slider-container', '.slide', '.previous', '.next')
