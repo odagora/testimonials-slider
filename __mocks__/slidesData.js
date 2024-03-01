a5ca6af01f18a05daae6a12f65eb78480996a3c6
Daniel <oscar.d.gonzalez@gmail.com>
2024-03-01 16:54:28 -0500

    test: :white_check_mark: 'Slider' class unit test suite (w/SliderItem web component)

---
+++ __mocks__/slidesData.js
@@ -0,0 +1,18 @@
+export const mockSlidesData = [
+  {
+    id: 1,
+    name: 'Mocked Name 1',
+    jobTitle: 'Mocked Job Title 1',
+    imageSrc: 'mocked_image_1.png',
+    imageAlt: 'Mocked Image 1',
+    quoteText: `Mocked quote text 1`
+  },
+  {
+    id: 2,
+    name: 'Mocked Name 2',
+    jobTitle: 'Mocked Job Title 2',
+    imageSrc: 'mocked_image_2.png',
+    imageAlt: 'Mocked Image 2',
+    quoteText: `Mocked quote text 2`
+  }
+]
--- __tests__/slider.test.js
+++ __tests__/slider.test.js
@@ -1,16 +1,13 @@
-import { Slider } from '../js/index'
+import { Slider } from '../js/components/slider'
+import { mockSlidesData as slides } from "../__mocks__/slidesData";
 
 describe('Slider', () => {
-  let slider
-  let dom
+  let slider, dom, sliderContainer, previousButton, nextButton
 
   beforeEach(() => {
     // Arrange
     const html = `
-      <div class="slider-container">
-        <article class="slide" id="slide-1"></article>
-        <article class="slide" id="slide-2"></article>
-      </div>
+      <div class="slider-container"></div>
       <div class="slider-controls">
         <button class="previous" aria-label="Previous"></button>
         <button class="next" aria-label="Next"></button>
@@ -19,58 +16,59 @@ describe('Slider', () => {
     const parser = new DOMParser();
     dom = parser.parseFromString(html, 'text/html');
 
-    // Act
+    sliderContainer = dom.querySelector('.slider-container');
+    previousButton = dom.querySelector('.previous');
+    nextButton = dom.querySelector('.next');
+
+    slider = new Slider({ sliderContainer, previousButton, nextButton, slides})
+
     jest.spyOn(document, 'querySelector').mockImplementation((selector) => {
       if (selector === '.slider-container') {
-        return dom.querySelector('.slider-container');
-      } else if (selector === '.previous') {
-        return dom.querySelector('.previous');
-      } else if (selector === '.next') {
-        return dom.querySelector('.next');
+        return sliderContainer;
       }
-      return null;
     })
-
-    jest.spyOn(document, 'querySelectorAll').mockImplementation((selector) => {
-      if (selector === '.slide') {
-        return dom.querySelectorAll('.slide');
-      }
-      return [];
-    })
-
-    slider = new Slider('.slider-container', '.slide', '.previous', '.next')
   })
 
   afterEach(() => {
     jest.resetAllMocks()
   })
 
-  test('should initialize slider with the correct selectors', () => {
+  test('should initialize slider with all elements', () => {
+    // Act
+    slider.init();
+
     // Assert
-    expect(slider.sliderContainer).toBeDefined();
-    expect(slider.slides).toBeDefined();
-    expect(slider.previousButton).toBeDefined();
-    expect(slider.nextButton).toBeDefined();
+    expect(slider.sliderContainer).toBe(sliderContainer);
+    expect(slider.previousButton).toBe(previousButton);
+    expect(slider.nextButton).toBe(nextButton);
+    expect(slider.currentSlide).toBe(0);
+    expect(slider.slides).toBe(slides);
+    expect(slider.numberOfSlides).toBe(slides.length);
   })
 
   test('should initialize slider with disabled previous button', () => {
+    // Act
     slider.init();
+
+    // Assert
     expect(slider.previousButton.classList.contains('disabled')).toBe(true);
   })
 
-  test('should set the \'current slide\' to zero and the number of slides correctly on first load', () => {
+  test('should add event listeners to previous and next buttons', () => {
+    // Arrange
+    // Spy on addEventListener function
+    const previousButtonEventListenerSpy = jest.spyOn(slider.previousButton, 'addEventListener')
+    const nextButtonEventListenerSpy = jest.spyOn(slider.nextButton, 'addEventListener')
+
+    // Act
+    slider.addEventListeners();
+
     // Assert
-    expect(slider.currentSlide).toBe(0);
-    expect(slider.numberOfSlides).toBe(2);
-    expect(slider.slides[0].id).toBe('slide-1');
-    expect(slider.slides[1].id).toBe('slide-2');
+    expect(previousButtonEventListenerSpy).toHaveBeenCalledWith('click', expect.any(Function));
+    expect(nextButtonEventListenerSpy).toHaveBeenCalledWith('click', expect.any(Function));
   })
 
   test('should transition to the next slide and disable the next button when on the last slide', () => {
-    // Arrange
-    const nextButton = dom.querySelector('.next');
-    const sliderContainer = dom.querySelector('.slider-container');
-
     // Act
     slider.init();
     nextButton.click();
@@ -82,11 +80,6 @@ describe('Slider', () => {
   });
 
   test('should transition to the previous slide and disable the previous button when on the first slide', () => {
-    //Arrange
-    const nextButton = dom.querySelector('.next');
-    const previousButton = dom.querySelector('.previous');
-    const sliderContainer = dom.querySelector('.slider-container');
-
     // Act
     slider.init();
     nextButton.click();
